72b1be4c6ccb5414f945c8debc3d6cc7a19611af
mohit <mahormohit1299@gmail.com>
2024-04-18 00:46:03 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello World !!</h1>
+     <AddTodo />
+     <Todos />
+    </>
+  )
+}
+
+export default App
